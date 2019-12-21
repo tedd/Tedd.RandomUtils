ede6bd22df19f94e76d71542cf026e2137bba7ed
Tedd Hansen <tedd@konge.net>
2019-12-21 13:53:06 +0100

    100% unit test coverage on .Net Core

--- src/Tedd.RandomUtils.Tests/ConcurrentRandom/AverageTest.cs
+++ src/Tedd.RandomUtils.Tests/ConcurrentRandom/AverageTest.cs
@@ -11,13 +11,99 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         private int iterations = 100_000;
         private float tolerance = 0.1f;
 
+        [Fact]
+        public void TestNext()
+        {
 
- 
+            for (var c = 0; c < count; c++)
+            {
+                BigInteger sum = 0;
+                for (var i = 0; i < iterations; i++)
+                {
+                    sum += rnd.Next();
+                }
+
+                sum /= iterations;
+
+                var mid = Int32.MaxValue / 2;
+                Assert.InRange((Int32)sum, mid - (Int32.MaxValue * tolerance), mid + (Int32.MaxValue * tolerance));
+            }
+        }
+
+        [InlineData(1)]
+        [InlineData(10_000)]
+        [InlineData(100_000)]
+        [Theory]
+        public void TestNext_max(int max)
+        {
+
+            for (var c = 0; c < count; c++)
+            {
+                BigInteger sum = 0;
+                for (var i = 0; i < iterations; i++)
+                {
+                    sum += rnd.Next(max);
+                }
+
+                sum /= iterations;
+
+                var mid = max / 2;
+                Assert.InRange((Int32)sum, mid - (max * tolerance), mid + (max * tolerance));
+            }
+        }
+
+        [InlineData(-1, 1)]
+        [InlineData(-500, 1_000)]
+        [InlineData(9_999, 10_000)]
+        [InlineData(Int32.MinValue, 100_000)]
+        [Theory]
+        public void TestNext_min_max(int min, int max)
+        {
+
+            for (var c = 0; c < count; c++)
+            {
+                BigInteger sum = 0;
+                for (var i = 0; i < iterations; i++)
+                {
+                    sum += rnd.Next(min, max);
+                }
+
+                sum /= iterations;
+
+                var range = (Int64)max - (Int64)min;
+                var mid = (Int64)min + range / 2;
+                Assert.InRange((Int64)sum, mid - (range * tolerance), mid + (range * tolerance));
+            }
+        }
+
+        [InlineData(1_000)]
+        [InlineData(10_000)]
+        [InlineData(100_000)]
+        [Theory]
+        public void TestNextBytes(int size)
+        {
+            for (var c = 0; c < count; c++)
+            {
+                BigInteger sum = 0;
+                var array = new byte[size];
+                rnd.NextBytes(array);
+                Assert.Equal(size, array.Length);
+
+                for (var i = 0; i < size; i++)
+                {
+                    sum += array[i];
+                }
+
+                sum /= size;
+
+                Assert.InRange((int)sum, 120, 140);
+            }
+        }
 
         [Fact]
         public void TestBoolean()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 var t = 0;
@@ -39,7 +125,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestFloat()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -52,9 +138,39 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         }
 
         [Fact]
+        public void TestDouble()
+        {
+
+            for (var c = 0; c < count; c++)
+            {
+                BigInteger sum = 0;
+                for (var i = 0; i < iterations; i++)
+                    sum += (Int64)rnd.NextDouble();
+                sum /= iterations;
+                var mid = (Double.MinValue + Double.MaxValue) / 2;
+                Assert.InRange((Int64)sum, (Int64)(mid - Double.MaxValue * 2 * tolerance), (Int16)(mid + Double.MaxValue * 2 * tolerance));
+            }
+        }
+
+        [Fact]
+        public void TestSample()
+        {
+
+            for (var c = 0; c < count; c++)
+            {
+                BigInteger sum = 0;
+                for (var i = 0; i < iterations; i++)
+                    sum += (Int64)rnd.Sample();
+                sum /= iterations;
+                var mid = (Double.MinValue + Double.MaxValue) / 2;
+                Assert.InRange((Int64)sum, (Int64)(mid - Double.MaxValue * 2 * tolerance), (Int16)(mid + Double.MaxValue * 2 * tolerance));
+            }
+        }
+
+        [Fact]
         public void TestSByte()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -69,7 +185,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestByte()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -84,7 +200,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestInt16()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -99,7 +215,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestUInt16()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -115,7 +231,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestInt32()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -130,7 +246,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestUInt32()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -146,7 +262,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestInt64()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -162,7 +278,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestUInt64()
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 BigInteger sum = 0;
@@ -183,7 +299,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Theory]
         public void TestString_String(string chars, int length)
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 var str = rnd.NextString(chars, length);
@@ -199,7 +315,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Theory]
         public void TestString_Chars(string chars, int length)
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 var str = rnd.NextString(chars.ToCharArray(), length);
@@ -215,7 +331,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Theory]
         public void TestString_Span(string chars, int length)
         {
-            
+
             for (var c = 0; c < count; c++)
             {
                 var str = rnd.NextString(new ReadOnlySpan<char>(chars.ToCharArray()), length);
@@ -229,7 +345,7 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestString_ArgumentOutOfRangeException()
         {
-            
+
             Assert.Throws<ArgumentOutOfRangeException>(() => rnd.NextString("abc", -1));
         }
 
@@ -237,8 +353,8 @@ namespace Tedd.RandomUtils.Tests.ConcurrentRandom
         [Fact]
         public void TestString_Empty()
         {
-            
-            Assert.Equal(string.Empty,rnd.NextString("abc", 0));
+
+            Assert.Equal(string.Empty, rnd.NextString("abc", 0));
         }
     }
 #endif
